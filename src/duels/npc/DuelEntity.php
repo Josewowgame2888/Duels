5f4325af70c5505a5663aadb85a32dc4df318d5f
Josewowgame2888 <mistertec7777777@gmail.com>
2019-11-16 12:02:23 -0600

    ticks update

--- src/duels/npc/DuelEntity.php
+++ src/duels/npc/DuelEntity.php
@@ -9,11 +9,13 @@ class DuelEntity extends Human
 
     public function entityBaseTick($tickDiff = 1)
     {
+        $return = parent::entityBaseTick($tickDiff);
         $version = Duels::getMain()->getDescription()->getVersion();
         if(strlen($version) <= 1)
         {
             $version .= '.0.0';
         }
         $this->setNameTag('§bClassic Duels §7[v'.$version.']'."\n".'§e'.Duels::getArena()->getPlaying().' Players'."\n".'§l§aCLICK TO PLAY');
+        return $return;
     }
 }
